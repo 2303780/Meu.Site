c8240556b2c3ca77b9ef53ddce0b604d2b36bcef
2303780 <2303780@escolas.anchieta.br>
2024-10-23 22:09:22 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -12,3 +12,10 @@ st.subheader("Sub Cabeçalho")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+st.subheader("This is a subheader with a divider", divider="gray")
+st.subheader("These subheaders have rotating dividers", divider=True)
+st.subheader("One", divider=True)
+st.subheader("Two", divider=True)
+st.subheader("Three", divider=True)
+st.subheader("Four", divider=True)
