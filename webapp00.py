c58c4206a644973bdb4fc50c89985a081fe4c151
2303780 <2303780@escolas.anchieta.br>
2024-10-23 22:08:38 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -12,19 +12,3 @@ st.subheader("Sub Cabeçalho")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-st.title('st.session_state')
-
-def lbs_to_kg():
-  st.session_state.kg = st.session_state.lbs/2.2046
-def kg_to_lbs():
-  st.session_state.lbs = st.session_state.kg*2.2046
-
-st.header('Input')
-col1, spacer, col2 = st.columns([2,1,2])
-with col1:
-  pounds = st.number_input("Pounds:", key = "lbs", on_change = lbs_to_kg)
-with col2:
-  kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs)
-
-st.header('Output')
-st.write("st.session_state object:", st.session_state)
